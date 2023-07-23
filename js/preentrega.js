d85e2a4551f89710d8d8da7046ab246617e06bd6
marianC <curotto.mariano@gmail.com>
2023-07-23 21:10:18 +0200

    entrega final

--- js/preentrega.js
+++ js/preentrega.js
@@ -1,92 +1,123 @@
-let productos = [
-  {
-    id: 1,
-    nombre: "aston martin",
-    categoria: "ingles",
-    precio: 1500,
-    img: "./img/astonMartin.jfif"
-  },
-  {
-    id: 2,
-    nombre: "ferrari",
-    categoria: "italiano",
-    precio: 2300,
-    img: "./img/ferrari.jfif"
-  },
-  {
-    id: 3,
-    nombre: "jaguar",
-    categoria: "ingles",
-    precio: 10000,
-    img: "./img/jaguar.jpg"
-  },
-  {
-    id: 4,
-    nombre: "Mercedes benz",
-    categoria: "Aleman",
-    precio: 200,
-    img: "./img/mercedesBenz.webp",
-  },
-  {
-    id: 5,
-    nombre: "Maserati",
-    categoria: "italia",
-    precio: 2000,
-    img: "./img/maserati.jpg",
-  },
-  {
-    id: 6,
-    nombre: "Ford",
-    categoria: "Estadounidence",
-    precio: 1200,
-    img: "./img/ford.jpg",
-  },
-  {
-    id: 7,
-    nombre: "jeep",
-    categoria: "Estadounidence",
-    precio: 2300,
-    img: "./img/jeep.jpg",
-  },
-  {
-    id: 8,
-    nombre: "Audi",
-    categoria: "Aleman",
-    precio: 22200,
-    img: "./img/audi.webp",
-  }
+// let productos = [
+//   {
+//     id: 1,
+//     nombre: "aston martin",
+//     categoria: "ingles",
+//     precio: 1500,
+//     img: "./img/astonMartin.jfif"
+//   },
+//   {
+//     id: 2,
+//     nombre: "ferrari",
+//     categoria: "italiano",
+//     precio: 2300,
+//     img: "./img/ferrari.jfif"
+//   },
+//   {
+//     id: 3,
+//     nombre: "jaguar",
+//     categoria: "ingles",
+//     precio: 10000,
+//     img: "./img/jaguar.jpg"
+//   },
+//   {
+//     id: 4,
+//     nombre: "Mercedes benz",
+//     categoria: "Aleman",
+//     precio: 200,
+//     img: "./img/mercedesBenz.webp",
+//   },
+//   {
+//     id: 5,
+//     nombre: "Maserati",
+//     categoria: "italia",
+//     precio: 2000,
+//     img: "./img/maserati.jpg",
+//   },
+//   {
+//     id: 6,
+//     nombre: "Ford",
+//     categoria: "Estadounidence",
+//     precio: 1200,
+//     img: "./img/ford.jpg",
+//   },
+//   {
+//     id: 7,
+//     nombre: "jeep",
+//     categoria: "Estadounidence",
+//     precio: 2300,
+//     img: "./img/jeep.jpg",
+//   },
+//   {
+//     id: 8,
+//     nombre: "Audi",
+//     categoria: "Aleman",
+//     precio: 22200,
+//     img: "./img/audi.webp",
+//   }
 
-]
+// ]
 let botonCarrito = document.getElementById('boton-carrito')
 let modalContainer = document.getElementById ('modal-container')
 let carrito = JSON.parse(localStorage.getItem('carrito')) || []
 const saveLocal = () => {localStorage.setItem('carrito', JSON.stringify(carrito))};
-productos.map(producto => {
-  let card = document.createElement('div');
-  card.classList.add('card', 'col-md-4', 'mb-4'); 
 
-  card.innerHTML = `
-    <div class="card-body, border, bc">
-      <h3 class="card-title, p20">${producto.nombre}</h3>
-      <img src="${producto.img}" class="card-img-top">
-      <h4 class="card-subtitle, p20, center">País de origen: ${producto.categoria}</h4>
-      <p class="card-text, p20, center">Precio en pesos: ${producto.precio}</p>
-    </div>
-  `;
 
-  container.append(card);
-  let boton = document.createElement ('button')
-  boton.innerText = 'Agregar al carrito'
-  card.append(boton)
-  boton.addEventListener ('click', () =>{
-    carrito.push ({
-      id: producto.id,
-      nombre: producto.nombre,
-      precio: producto.precio,
+function mostrarCard (array){
+  array.map(producto => {
+    let card = document.createElement('div');
+  
+    card.innerHTML = `
+      <div class="card">
+        <h3 class="">${producto.title}</h3>
+        <img src="${producto.image}" class="img">
+        <h4 class="">Categoria: ${producto.category}</h4>
+        <p class="">Precio en dolareques verdes ricos: ${producto.price}</p>
+      </div>
+    `;
+  
+    container.append(card);
+    let boton = document.createElement ('button')
+    boton.innerText = 'Agregar al carrito'
+    boton.className = 'boton-comprar'
+    card.append(boton)
+    boton.addEventListener ('click', () =>{
+      carrito.push ({
+        id: producto.id,
+        nombre: producto.title,
+        precio: producto.price,
+      })
+      saveLocal();
     })
-    saveLocal();
-  })
-});
+  });
+ 
+}
+// productos.map(producto => {
+//   let card = document.createElement('div');
+//   card.classList.add('card', 'col-md-4', 'mb-4'); 
+
+//   card.innerHTML = `
+//     <div class="card-body, border, bc">
+//       <h3 class="card-title, p20">${producto.nombre}</h3>
+//       <img src="${producto.img}" class="card-img-top">
+//       <h4 class="card-subtitle, p20, center">País de origen: ${producto.categoria}</h4>
+//       <p class="card-text, p20, center">Precio en pesos: ${producto.precio}</p>
+//     </div>
+//   `;
+
+//   container.append(card);
+//   let boton = document.createElement ('button')
+//   boton.innerText = 'Agregar al carrito'
+//   card.append(boton)
+//   boton.addEventListener ('click', () =>{
+//     carrito.push ({
+//       id: producto.id,
+//       nombre: producto.nombre,
+//       precio: producto.precio,
+//     })
+//     saveLocal();
+//   })
+// });
 
 function mostrarCarrito (){
   modalContainer.style.display = 'flex'
@@ -102,9 +133,11 @@ function mostrarCarrito (){
   })
  
   let containerItems = document.createElement ('div')
+  containerItems.className ='container-items'
   modalContainer.append(containerItems) 
     carrito.map((producto)=>{
     let carrritoItem = document.createElement ('div')
+    carrritoItem.className ='cart-item'
     carrritoItem.innerHTML = `<p>${producto.nombre}</p><p>${producto.precio}</p>`
     containerItems.append(carrritoItem)
   })
@@ -217,3 +250,9 @@ function limpiarCarrito(){
   saveLocal()
 
 }
+function obtenerProductos(){
+fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=> mostrarCard(json))
+}
+obtenerProductos()
